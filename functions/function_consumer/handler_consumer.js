261e2e0e8166089de735a41557408373fcf1f1c5
Alejandro Soto <alesoto9113@gmail.com>
2020-12-11 15:51:27 -0300

    update

--- functions/function_consumer/handler_consumer.js
+++ functions/function_consumer/handler_consumer.js
@@ -3,12 +3,12 @@
 module.exports.consumer = function(event, context, callback) {
 
     console.log('Invoking consumer function');
-    console.log("A fact about cat: " + event.Records[0].body);
+    console.log("A fact about cat: ", event.Records);
 
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: event.Records[0].body
+            message: event.Records
         }),
     };
 
--- functions/function_producer/handler_producer.js
+++ functions/function_producer/handler_producer.js
@@ -7,33 +7,45 @@ var sqs = new AWS.SQS();
 module.exports.producer = function(event, context, callback) {
 
     console.log('Invoking producer function');
+    let params = {
+      MessageBody: `My message body ${new Date().getTime()}`,
+      QueueUrl: process.env.CAT_FACTS_QUEUE_URL
+  };
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+        console.log('Error encountered sending a message: %s', err);
+    } else {
+        console.log('A message about a cat fact is sent to ' + params.QueueUrl);
+    }
+});
 
-    httpClient.get('https://cat-fact.herokuapp.com/facts', (resp) => {
-      let data = '';
+callback(null, {m: 'OK'});
+    // httpClient.get('https://cat-fact.herokuapp.com/facts', (resp) => {
+    //   let data = '';
 
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+    //   resp.on('data', (chunk) => {
+    //     data += chunk;
+    //   });
 
-      resp.on('end', () => {
-        let facts = JSON.parse(data);
-        for (var i = 0; i < facts.all.length; i++) {
-            let params = {
-                MessageBody: facts.all[i].text,
-                QueueUrl: process.env.CAT_FACTS_QUEUE_URL
-            };
+    //   resp.on('end', () => {
+    //     let facts = JSON.parse(data);
+    //     for (var i = 0; i < facts.all.length; i++) {
+    //         let params = {
+    //             MessageBody: facts.all[i].text,
+    //             QueueUrl: process.env.CAT_FACTS_QUEUE_URL
+    //         };
 
-            sqs.sendMessage(params, function(err, data) {
-                if (err) {
-                    console.log('Error encountered sending a message: %s', err);
-                } else {
-                    console.log('A message about a cat fact is sent to ' + params.QueueUrl);
-                }
-            });
-        }
-      });
+    //         sqs.sendMessage(params, function(err, data) {
+    //             if (err) {
+    //                 console.log('Error encountered sending a message: %s', err);
+    //             } else {
+    //                 console.log('A message about a cat fact is sent to ' + params.QueueUrl);
+    //             }
+    //         });
+    //     }
+    //   });
 
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
+    // }).on("error", (err) => {
+    //   console.log("Error: " + err.message);
+    // });
 };
